2e5db3bf0764e2e9a9b650bd851b05bf9061d3cb
Smith <Smith@antino.io>
2023-06-23 09:35:19 +0530

    Custom Notification using local Notification..

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,76 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:peacemaker/noti.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MaterialApp(home: Peacemaker()));
-}
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
-class Peacemaker extends StatefulWidget {
-  const Peacemaker({super.key});
-
-  @override
-  State<Peacemaker> createState() => _PeacemakerState();
-}
-
-void getfcmtoken() async {
+void main() async {
   final fcmToken = await FirebaseMessaging.instance.getToken();
-  print('Notifyme: $fcmToken');
+  runApp(const MyApp());
 }
 
-class _PeacemakerState extends State<Peacemaker> {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Notification")),
-      body: const Center(
-        child: Column(
-          children: [
-            ElevatedButton(onPressed: getfcmtoken, child: Text('Notify Me')),
-          ],
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Noti.initialize(flutterLocalNotificationsPlugin);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.blue.withOpacity(0.5),
+          ),
+          body: Center(
+            child: SizedBox(
+              width: 200,
+              height: 200,
+              child: Column(
+                children: [
+                  const ElevatedButton(
+                      onPressed: getFcmToken, child: Text('GetToken')),
+                  ElevatedButton(
+                    onPressed: () {
+                      Noti.showBigTextNotification(
+                          title: "Peacemaker",
+                          body: "You only live once..",
+                          fln: flutterLocalNotificationsPlugin);
+                    },
+                    child: const Text("Notify me"),
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+void getFcmToken() async {
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
 }
